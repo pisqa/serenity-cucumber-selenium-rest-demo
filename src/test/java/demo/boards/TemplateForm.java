db22629ab0d6aa8ee969eb68e0bfb70e341cde7b
Paul Igoe <57501449+pisqa@users.noreply.github.com>
2020-02-20 18:49:19 +0100

    Add files via upload

--- src/test/java/demo/boards/TemplateForm.java
+++ src/test/java/demo/boards/TemplateForm.java
@@ -6,7 +6,7 @@ class TemplateForm {
 
 	static By TEMPLATES_BUTTON = By.xpath("//li[@data-test-id='templates']");
 	static By TEMPLATES_SEARCH_FIELD = By.xpath("//input[@id='react-select-2-input']");
-	static By TEMPLATES_VIEW_BUTTON = By.xpath("//button[text()='View Template']");
+	static By TEMPLATES_VIEW_BUTTON = By.xpath("//a[text()='View Template']");
 	static By CREATE_BOARD_FROM_TEMPLATE_BUTTON = By.xpath("//a[@title ='Create Board from Template']");
 	static By NEW_BOARD_TITLE_FIELD = By.xpath("//input[@id ='boardNewTitle']");
 	static By NEW_BOARD_KEEP_CARDS_CHECKBOX = By.xpath("//label[@for ='idKeepCards']");
